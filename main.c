fce32998c4342999c54711a82367d277e4d82e98
BHAVANEESHWARI H <bhavaneeshwari1110@gmail.com>
2026-02-11 07:57:01 +0530

    command output
    Removed help command details from print_help function.

--- main.c
+++ main.c
@@ -6,19 +6,13 @@
 
 void print_help(void)
 {
-    xil_printf("\r\n=== Available Commands ===\r\n");
-    xil_printf("READ <addr>                    - Read from memory address\r\n");
-    xil_printf("WRITE <addr> <data>            - Write data to memory address\r\n");
-    xil_printf("ARRAY_WRITE <addr> <cnt> <d0> <d1> ... - Write array to memory\r\n");
-    xil_printf("ARRAY_READ <addr> <cnt>        - Read array from memory\r\n");
-    xil_printf("DUMP <addr> <cnt>              - Dump memory contents\r\n");
-    xil_printf("\r\nExamples:\r\n");
+  
     xil_printf("  READ 0x10\r\n");
     xil_printf("  WRITE 0x20 0xDEADBEEF\r\n");
     xil_printf("  ARRAY_WRITE 0x00 4 0x11 0x22 0x33 0x44\r\n");
     xil_printf("  ARRAY_READ 0x00 4\r\n");
     xil_printf("  DUMP 0x00 16\r\n");
-    xil_printf("==========================\r\n");
+    
 }
 
 int main(void)
@@ -28,7 +22,6 @@ int main(void)
 
     uart_init();
 
-    /* Initialize command register to IDLE state */
     REG_COMMAND_WRITE(MAKE_COMMAND(0, STATUS_IDLE));
 
 
@@ -46,35 +39,27 @@ int main(void)
 
         xil_printf("\r\n>> ");
         
-        /* Read command from UART */
         uart_getline(cmd_buf, sizeof(cmd_buf));
         
-        /* Check for help command */
         if (!strcmp(cmd_buf, "HELP") || !strcmp(cmd_buf, "help")) {
             print_help();
             continue;
         }
         
-        /* Parse command and write to registers */
         parse_and_store(cmd_buf);
         
-        /* Poll until command completes */
         while (1) {
-            /* Let executor poll and process commands */
             executor_poll();
             
-            /* Check if done */
             cmd_status = GET_STATUS(REG_COMMAND_READ());
             if (cmd_status == STATUS_DONE) {
                 break;
             }
         }
         
-        /* Print final result register */
         xil_printf("\r\n[MAIN] Command completed.\r\n");
         xil_printf("[MAIN] Final REG_DATA = 0x%08x\r\n", REG_DATA_READ());
         
-        /* Reset to IDLE */
         REG_COMMAND_WRITE(MAKE_COMMAND(0, STATUS_IDLE));
     }
 
